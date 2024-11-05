96579de58ee78cedc31dd2dbd1d29d0bb747841c
devkoow <cwl64658@gmail.com>
2024-11-05 13:36:41 +0900

    [Refactor] 글로벌 메타데이터 설정

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { wrapper } from '@/utils/redux/store';
 import { theme } from '@/defaultTheme';
+import Head from 'next/head';
 import Layout from '@/layouts/Layout';
 import ProtectedRoute from '@/components/ProtectedRoute';
 
@@ -23,6 +24,26 @@ function MyApp({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <QueryClientProvider client={queryClient}>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <meta name="author" content="windowook" />
+            <meta name="description" content="미스터 크립 : 크립토 비서" />
+            <meta
+              name="keywords"
+              content="미스터 크립, 미스터크립, mr cryp, mr-cyrp"
+            />
+            <meta property="og:title" content="미스터 크립" />
+            <meta
+              property="og:description"
+              content="미스터 크립 : 크립토 비서"
+            />
+            <meta property="og:image" content="/image/og-image.webp" />
+            <meta property="og:url" content="https://mr-cryp.vercel.app" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
           {isSignIn || isKakaoRedirecting || isNaverRedirecting ? (
             <Component {...pageProps} />
           ) : (
