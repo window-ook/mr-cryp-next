0ed80bdc3cbe20c9727ca6ed2e9adbd6ffcca2f7
devkoow <cwl64658@gmail.com>
2024-11-11 12:26:02 +0900

    [Refactor] document.js에 글로벌 Head 설정, 404.jsx에 Link 삭제

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -25,25 +25,7 @@ function MyApp({ Component, pageProps }) {
         <CssBaseline />
         <QueryClientProvider client={queryClient}>
           <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <meta name="author" content="windowook" />
-            <meta name="description" content="미스터 크립 : 크립토 비서" />
-            <meta
-              name="keywords"
-              content="미스터 크립, 미스터크립, mr cryp, mr-cyrp"
-            />
-            <meta property="og:title" content="미스터 크립" />
-            <meta
-              property="og:description"
-              content="미스터 크립 : 크립토 비서"
-            />
-            <meta property="og:image" content="/image/og-image.webp" />
-            <meta property="og:url" content="https://mr-cryp.vercel.app" />
-            <link rel="icon" href="/favicon.ico" />
-            <title>미스터 크립 | Mr.cryp</title>
+            <title>미스터 크립 Mr.cryp</title>
           </Head>
           {isSignIn || isKakaoRedirecting || isNaverRedirecting ? (
             <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="author" content="windowook" />
+        <meta name="description" content="미스터 크립 : 크립토 비서" />
+        <meta
+          name="keywords"
+          content="미스터 크립, 미스터크립, mr cryp, mr-cyrp"
+        />
+        <meta property="og:title" content="미스터 크립" />
+        <meta property="og:description" content="미스터 크립 : 크립토 비서" />
+        <meta property="og:image" content="/image/og-image.webp" />
+        <meta property="og:url" content="https://mr-cryp.vercel.app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
