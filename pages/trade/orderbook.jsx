1592117a20560465c684c0df89ba78316e21c59a
devkoow <cwl64658@gmail.com>
2024-11-13 15:27:17 +0900

    [Refactor] TableAlone 컴포넌트 코드 스플리팅

--- pages/trade/orderbook.jsx
+++ pages/trade/orderbook.jsx
@@ -1,7 +1,15 @@
 import { memo, useEffect, useState } from 'react';
+import { LinearProgress } from '@mui/material';
 import axios from 'axios';
-import OrderbookTableAlone from '@/components/trade/orderbook/OrderbookTableAlone';
+import dynamic from 'next/dynamic';
 
+const OrderbookTableAlone = dynamic(
+  () => import('@/components/trade/orderbook/OrderbookTableAlone'),
+  {
+    ssr: false,
+    loading: () => <LinearProgress color="primary" />,
+  },
+);
 export async function getStaticProps() {
   const domain = process.env.NEXT_PUBLIC_API_URL;
   let marketCodes = [];
